37349a2045c72feca23f866983cddd9daf4df198
adv-11 <advaitss11@gmail.com>
2025-02-22 12:36:27 +0530

    rough draft

---
+++ app.py
@@ -0,0 +1,21 @@
+
+# Input Repository and getting the Git Ingest 
+
+
+# Human in Loopp Validation  - > human provides Expectations and Guidelines
+
+
+# Code review ( Model : )      -> Input is prev two : 1) Repo Codebase  2) Expec + Guidelines
+
+
+# Migration Strategy -> RLHF
+
+
+# Approval logic by huamn
+
+
+
+
+
+
+#----------------------------------------------- Migration starts here -------------------------------
